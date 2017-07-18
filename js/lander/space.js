1c3d1a6a8c5d9f8171370830619d55fa9a866a75
egonjouda <egonjouda@yahoo.com.au>
2017-07-18 17:39:54 +0000

    f

--- js/lander/space.js
+++ js/lander/space.js
@@ -1,22 +1,27 @@
 var scene, transparentBackground, spaceImages = ["img/lander/background/Background-2.jpg", "img/lander/background/Background-3.jpg", "img/lander/background/Background-4.jpg", "img/lander/background/Background-5.jpg", "img/lander/background/Background-6.jpg", "img/lander/background/Background-7.jpg", "img/lander/background/Background-8.jpg", "img/lander/background/Background-9.jpg", "img/lander/background/Background-10.jpg", "img/lander/background/Background-11.jpg", "img/lander/background/Background-12.jpg", "img/lander/background/Background-13.jpg", "img/lander/background/Background-14.jpg"],
-    lander, platform, spacemoths, spacecrabs = [], platforms = [], life_250, message = "", spaceMP3, spaceOGG, moth_crashMP3, moth_crashOGG, life_250MP3, life_250OGG, NUMSPACEMOTHS = 3, fuel = 12000, level = 1,
+    lander, platform, spacemoths, spacecrabs = [], plants = [], life_250, message = "", spaceMP3, spaceOGG, moth_crashMP3, moth_crashOGG, life_250MP3, life_250OGG, NUMSPACEMOTHS = 3, fuel = 12000, level = 1,
     dirs = [["left",    270],
             ["right",   90 ],
             ["up",      0  ],
             ["down",    180]];
-            var NUMOFSPACECRABS = 50; var dead = 0;
+var NUMOFSPACECRABS = 250; var dead = 0; var NUM_OF_PLANTS = 500;
 function populateRandomCrab() {
     Math.random().toString(2)[7] == 0 ? spacecrabs.push(new Crabella()) : spacecrabs.push(new Crabber()); //toString(2) returns binary rep - check last binary if it 0 or 1
 }
-                
+
+var VISIBLE_CRABS_AT_THE_BEGINNING = 30;               
 function init() {
     scene = new Scene();
     scene.setSize(window.innerWidth, window.innerHeight);
     scene.setBG("transparent");
     for (var i = 0; i < NUMOFSPACECRABS; i++) {populateRandomCrab();}
+    for (var i = 0; i < VISIBLE_CRABS_AT_THE_BEGINNING; i++) {
+        spacecrabs[i].show(); spacecrabs[i].playAnimation(); spacecrabs[i].healthBar.show(); spacecrabs[i].healthBarFollow();
+        spacecrabs[i].setPosition(Math.random() * spacecrabs[i].cWidth, Math.random() * spacecrabs[i].cHeight);
+        }
+    for (var i = 0; i < NUM_OF_PLANTS; i++) {plants.push(new Plant());}
     
     stats = document.getElementById("game_stats");
-    
     
     moth_crashMP3 = new Sound("sounds/moth_crash.mp3");
     moth_crashOGG = new Sound("sounds/moth_crash.ogg");
@@ -24,87 +29,203 @@ function init() {
     life_250OGG = new Sound("sounds/life_250.ogg");
     scene.start();
 } // end ini
+
 var index = 0;
-var k = NUMOFSPACECRABS;
+var crab;
+var plant;
+var visiblePlants = 0;
+var visibleCrabs = 0;
 function update() {
     scene.clear();
-    spacecrabs[index++].changeDirection();// update one at a time
-    for(var i = 0; i < spacecrabs.length; i++)
-    {
-        spacecrabs[i].healthBarFollow();
-    }
-        
-    if (index >= NUMOFSPACECRABS)
-    {
-        index = 0; // reset
-    }
-    while(k--) {
-        checkCrabCollisions(k);
-        if(spacecrabs[k].pregnant){
-            spacecrabs[k].getBirth();
+    
+    var crabsCount = 0;
+    crab = NUMOFSPACECRABS;
+    while(crab--) {
+        var currentCrab = spacecrabs[crab];
+        if (currentCrab.visible) {
+            crabsCount++;
+            if (currentCrab.countToChageDirection-- < 0) {
+                currentCrab.changeDirection();
+                currentCrab.countToChageDirection = 40;
+            }
+            currentCrab.healthBarFollow();
+            checkCrabCollisions(crab);
+            checkCrabPlantCollisions(crab);
+            
+            currentCrab.getOlder();
+            if(currentCrab.pregnant){
+                currentCrab.getBirth();
+            }
+            currentCrab.update();
+            currentCrab.healthBar.update();
+        };
+    };
+    visibleCrabs = crabsCount;
+    
+    plant = NUM_OF_PLANTS;
+    var newborn = false; // only 1 in iterration
+    var plantsCount = 0;
+    while(plant--) {
+        var currentPlant = plants[plant];
+        if (currentPlant.visible) {
+            plantsCount++;
+            if (currentPlant.nutrition >= 100) {
+                currentPlant.reset();
+            }
+        currentPlant.nutrition++;
+        currentPlant.update();
+        }else{
+            if ((visiblePlants < NUM_OF_PLANTS) &&  (!newborn)) {
+                currentPlant.show();
+                currentPlant.playAnimation();
+                newborn = true;
+            }
         }
-        spacecrabs[k].update();
-        spacecrabs[k].healthBar.update();
     }
-    k = NUMOFSPACECRABS;
+    visiblePlants = plantsCount;
     showStats();
 } // end update
-var showStats = function(){
-    //displays stats
-    output = "DX: " + Math.round(this.dx * 10) + "<br />";
-    output += "DY: " + Math.round(this.dy * 10) + "<br />";
-    output += "ALT: " + Math.round(525 - this.y) + "<br />";
-    output += "Dead: " + dead + "<br />";
-    output += "Creatues: " + NUMOFSPACECRABS + " and " + spacecrabs.length;
-    
-    stats.innerHTML = output;
-};
 
+function BaseCrab(image){
+    var tBaseCrab = new Sprite(scene, image, 132, 128);
+    tBaseCrab.getOlder = function(){
+        this.healthBar.setCurrentCycle(parseInt(Math.floor(this.health)/10));// reduce health and adjust sprite image to correct cycle if necessary
+        if ((this.health -= 0.05) < 1){
+            this.reset();
+            dead++;
+        }
+    };
+    tBaseCrab.changeDirection = function(){
+        var direction = dirs[Math.floor(Math.random()*dirs.length)];
+        this.setMoveAngle(direction[1]);
+        this.setCurrentCycle(direction[0]);
+    };
+    
+    tBaseCrab.healthBarFollow = function(){
+        this.healthBar.setRandomPosition(this.x, this.y);
+    };
+    
+    tBaseCrab.reset = function(){
+        this.healthBar = new Bar(this.x, this.y);
+        this.loadAnimation(132, 128, 33, 32);
+        this.generateAnimationCycles();
+        this.renameCycles(new Array("down", "up", "left", "right"));
+        this.setAnimationSpeed(500);
+        this.health = 100;
+        this.countToChageDirection = Math.floor(Math.random()*40);
+    
+            //start paused
+        this.setSpeed(1);
+        //set new random position
+        //newX = Math.random() * this.cWidth;
+        //newY = Math.random() * this.cHeight;
+        this.changeDirection();
+        this.pauseAnimation();
+        this.setPosition(-1000, -1000);
+        this.hide();
+        this.healthBar.hide();
+    }; // end reset
+
+    return tBaseCrab;
+} // end tBaseCrab
+
+function Crabella(){
+    var tCrabella = new BaseCrab("img/lander/crabella.png");
+    tCrabella.gender = "Crabella";
+    tCrabella.pregnant = false;
+    tCrabella.pregnancy = 100;
+    tCrabella.impregnate = function(){
+        this.pregnant = true;
+    };  
+    
+    tCrabella.getBirth = function(){
+        this.pregnancy--;
+        if (this.pregnancy < 1){
+            if (visibleCrabs < NUMOFSPACECRABS){
+                var crab = NUMOFSPACECRABS;
+                var newbornCrab = spacecrabs[--crab];
+                while (newbornCrab.visible && --crab){
+                    newbornCrab = spacecrabs[crab];
+                }
+                newbornCrab.show(); newbornCrab.playAnimation(); newbornCrab.healthBar.show(); newbornCrab.healthBarFollow();
+                newbornCrab.setPosition(Math.random() * newbornCrab.cWidth, Math.random() * newbornCrab.cHeight);
+                this.pregnancy = 100;
+                this.pregnant = false;
+            }
+        }
+    };
+
+    tCrabella.reset();
+    return tCrabella;
+} // end tCrabella
+
+function Crabber(){
+    var tCrabber = new BaseCrab("img/lander/crabber.png");
+    tCrabber.gender = "Crabber";
+
+    tCrabber.reset();
+    return tCrabber;
+} // end tCrabber
+
+function Plant(){
+    var tPlant = new Sprite(scene, "img/lander/plant.png", (window.innerWidth+window.innerHeight)/25, (window.innerWidth+window.innerHeight)/25);
+    
+    tPlant.reset = function(){
+        this.setSpeed(0);
+        this.nutrition = 0; // dies when 100
+        this.loadAnimation(468, 468, 26, 26);
+        this.generateAnimationCycles();
+        this.renameCycles(new Array("grow", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"));
+        this.setAnimationSpeed(5000);
+        this.setCurrentCycle("grow");
+        //set new random position
+        var newX = Math.random() * this.cWidth;
+        var newY = Math.random() * this.cHeight;
+        this.pauseAnimation();
+        this.setPosition(newX, newY);
+        this.hide();
+    }; // end reset
+    
+    tPlant.reset();
+    
+    return tPlant;
+} // end Plant
+
+function checkCrabPlantCollisions(spaceCrabNum) {// check crabs against each other
+    var plant = NUM_OF_PLANTS;
+    while(plant--) {
+        var currentPlant = plants[plant], currentCrab = spacecrabs[spaceCrabNum];
+        if (currentPlant.distanceTo(currentCrab) < 25) {
+            currentCrab.health = Math.min(currentCrab.health + currentPlant.nutrition, 100);
+            currentPlant.reset();
+        } // end if
+    }
+} // end checkCollisions
 
 function checkCrabCollisions(spaceCrabNum) {// check crabs against each other
     var crab = NUMOFSPACECRABS;
-    while(crab--)
-    {
+    while(crab--) {
+        var currentCrab = spacecrabs[spaceCrabNum], iterratedCrab = spacecrabs[crab];
         if (crab == spaceCrabNum) continue; // dont collide with itself
-        if (spacecrabs[crab].distanceTo(spacecrabs[spaceCrabNum]) < 25) {
-            if(spacecrabs[crab].gender == "Crabber" && spacecrabs[spaceCrabNum].gender == "Crabber")
-            {    
-                moth_crashMP3.play();
-                moth_crashOGG.play();
-                if (spacecrabs[crab].health < 1){
-                    NUMOFSPACECRABS--;
-                    spacecrabs.splice(crab,1);
-                    dead++;
-                }else if (spacecrabs[spaceCrabNum].health < 1){
-                    NUMOFSPACECRABS--;
-                    spacecrabs.splice(spaceCrabNum,1);
-                    dead++;
-                }else{
-                    spacecrabs[crab].healthBar.setCurrentCycle(parseInt(Math.floor(spacecrabs[crab].health--)/10));// reduce health and adjust sprite image to correct cycle if necessary
-                    spacecrabs[spaceCrabNum].healthBar.setCurrentCycle(parseInt(Math.floor(spacecrabs[spaceCrabNum].health--)/10));
+        if (iterratedCrab.distanceTo(currentCrab) < 25) {
+            if(iterratedCrab.gender == "Crabber" && currentCrab.gender == "Crabber"){    
+                moth_crashMP3.play();moth_crashOGG.play();
+                iterratedCrab.health--;
+                currentCrab.health--;
+            } else if (iterratedCrab.gender == "Crabella" && currentCrab.gender == "Crabber") {
+                life_250MP3.play();life_250OGG.play();
+                if((!iterratedCrab.pregnant) && (visibleCrabs < NUMOFSPACECRABS)){
+                    iterratedCrab.impregnate();
                 }
-            }
-            else if (spacecrabs[crab].gender == "Crabella" && spacecrabs[spaceCrabNum].gender == "Crabber")
-            {
-                life_250MP3.play();
-                life_250OGG.play();
-                if (spacecrabs[crab].gender == "Crabella"){
-                    if(!spacecrabs[crab].pregnant){
-                        spacecrabs[crab].impregnate();
-                    }
+            } else if (currentCrab.gender == "Crabella" && iterratedCrab.gender == "Crabber") {
+                life_250MP3.play();life_250OGG.play();
+                if((!currentCrab.pregnant) && (visibleCrabs < NUMOFSPACECRABS)){
+                    currentCrab.impregnate();
                 }
-                else{
-                    if(!spacecrabs[spaceCrabNum].pregnant){
-                        spacecrabs[spaceCrabNum].impregnate();
-                    }
-                }
-            }
-            else
-            {
+            }else{
             }
         } // end if
     }
-    crab = NUMOFSPACECRABS;
 } // end checkCollisions
 
 function Bar(x, y){
@@ -115,7 +236,7 @@ function Bar(x, y){
     tBar.pauseAnimation();
     tBar.setCurrentCycle("10");
     tBar.setRandomPosition = function(x, y){
-    this.setPosition(x - 10, y - 20);
+        this.setPosition(x - 10, y - 20);
     };
     return tBar;
 } // end bar constructor
@@ -124,98 +245,16 @@ function restart() {
     document.location.href = "";
 } // end restart
 
-function Crabella(){
-    tSpaceCrab = new Sprite(scene, "img/lander/crabella.png", 132, 128);
-    tSpaceCrab.healthBar = new Bar(this.x, this.y);
-    tSpaceCrab.loadAnimation(132, 128, 33, 32);
-    tSpaceCrab.generateAnimationCycles();
-    tSpaceCrab.renameCycles(new Array("down", "up", "left", "right"));
-    tSpaceCrab.setAnimationSpeed(500);
-    tSpaceCrab.myTimer = new Timer();
-    tSpaceCrab.gender = "Crabella";
-    tSpaceCrab.health = 100;
-    tSpaceCrab.pregnant = false;
-    tSpaceCrab.pregnancy = undefined;
-
-        //start paused
-    tSpaceCrab.setSpeed(1);
-    tSpaceCrab.changeDirection = function(){
-        var direction = dirs[Math.floor(Math.random()*dirs.length)];
-        this.setMoveAngle(direction[1]);
-        this.setCurrentCycle(direction[0]);
-            this.myTimer.reset();
-    };
-    tSpaceCrab.impregnate = function(){
-        this.pregnant = true;
-        this.pregnancy = new Timer();
-    };  
+var showStats = function(){
+    //displays stats
+    output = "DX: " + Math.round(this.dx * 10) + "<br />";
+    output += "Crabs visible: " + visibleCrabs + "<br />";
+    output += "Plants visible: " + visiblePlants + "<br />";
+    output += "Dead: " + dead + "<br />";
+    output += "Creatues: " + NUMOFSPACECRABS + " and " + spacecrabs.length;
     
-    tSpaceCrab.getBirth = function(){
-        if (this.pregnancy.getElapsedTime() > 5){
-            this.pregnancy = undefined;
-            NUMOFSPACECRABS++;
-            populateRandomCrab();
-            this.pregnant = false;
-        }
-    }
-    
-    tSpaceCrab.healthBarFollow = function(){
-        this.healthBar.setRandomPosition(this.x, this.y)
-    };
-    
-    tSpaceCrab.reset = function(){
-        //set new random position
-        newX = Math.random() * this.cWidth;
-        newY = Math.random() * this.cHeight;
-        this.setPosition(newX, newY);
-        this.changeDirection();
-        this.healthBar.setRandomPosition(this.x, this.y)
-    }; // end reset
-    //alert(tSpaceCrab.x);
-
-    tSpaceCrab.reset();
-
-    return tSpaceCrab;
-} // end tSpaceCrab
-
-function Crabber(){
-    tSpaceCrab = new Sprite(scene, "img/lander/crabber.png", 132, 128);
-    tSpaceCrab.healthBar = new Bar(this.x, this.y);
-    tSpaceCrab.loadAnimation(132, 128, 33, 32);
-    tSpaceCrab.generateAnimationCycles();
-    tSpaceCrab.renameCycles(new Array("down", "up", "left", "right"));
-    tSpaceCrab.setAnimationSpeed(500);
-    tSpaceCrab.myTimer = new Timer();
-    tSpaceCrab.gender = "Crabber";
-    tSpaceCrab.health = 100;
-
-        //start paused
-    tSpaceCrab.setSpeed(1);
-    tSpaceCrab.changeDirection = function(){
-        var direction = dirs[Math.floor(Math.random()*dirs.length)];
-        this.setMoveAngle(direction[1]);
-        this.setCurrentCycle(direction[0]);
-            this.myTimer.reset();
-    };
-    
-    tSpaceCrab.healthBarFollow = function(){
-        this.healthBar.setRandomPosition(this.x, this.y)
-    };
-    
-    tSpaceCrab.reset = function(){
-        //set new random position
-        newX = Math.random() * this.cWidth;
-        newY = Math.random() * this.cHeight;
-        this.setPosition(newX, newY);
-        this.changeDirection();
-        this.healthBar.setRandomPosition(this.x, this.y)
-    }; // end reset
-    //alert(tSpaceCrab.x);
-
-    tSpaceCrab.reset();
-
-    return tSpaceCrab;
-} // end tSpaceCrab
+    stats.innerHTML = output;
+};
 
 var waitForFinalEvent = (function () {
     var timers = {};
